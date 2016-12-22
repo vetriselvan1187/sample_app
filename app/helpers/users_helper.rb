cbd8c31c5197a4ce794950aa89988131ff9038ce
Vetriselvan <vetriblogger1187@gmail.com>
2016-12-22 12:24:12 +0530

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def gravator_for(user)
+     gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+     gravator_url = "https://secure.gravator.com/avator/#{gravator_id}"
+     image_tag(gravator_url, alt: user.name, class: "gravator")
+  end
 end
